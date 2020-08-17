b77bb8497fff9f14f370b38a52992dc50b189475
Taylor Branson <taylor.a.branson@gmail.com>
2020-08-16 23:12:47 -0500

    added practice files

---
+++ src/practice/HelloWorld.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class HelloWorld {
+
+    private String message = "Hello, World";
+
+    public void sayHello() {
+        System.out.println(message);
+    }
+}
---
+++ src/practice/HelloWorldRunner.java
@@ -0,0 +1,9 @@
+package practice;
+
+public class HelloWorldRunner {
+
+    public static void main(String[] args) {
+        HelloWorld hello = new HelloWorld();
+        hello.sayHello();
+    }
+}
