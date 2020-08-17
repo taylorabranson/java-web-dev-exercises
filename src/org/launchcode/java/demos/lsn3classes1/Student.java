ec1fafe7841ab4dc55c6cf67e406e7194e2b6cd9
TaylorBranson <taylor.a.branson@gmail.com>
2020-08-17 17:07:58 -0500

    changed a few things

--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -1,7 +1,8 @@
 package org.launchcode.java.demos.lsn3classes1;
 
 // Start working here with your Student class.
-// To instantiate the Student class, add your code to the main in the file, SchoolPractice.
+// To instantiate the Student class, add your code to the main
+// in the file, SchoolPractice.
 
 public class Student {
 
