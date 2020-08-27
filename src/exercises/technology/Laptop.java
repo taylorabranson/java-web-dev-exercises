e5487467419f7f0a23ed018a0eea5230c874fa50
Taylor Branson <taylor.a.branson@gmail.com>
2020-08-27 16:34:58 -0500

    implemented Laptop class

--- src/exercises/technology/Laptop.java
+++ src/exercises/technology/Laptop.java
@@ -1,2 +1,50 @@
-package exercises.technology;public class Laptop {
+package exercises.technology;
+
+public class Laptop extends Computer {
+
+    private int size;
+    private boolean isUpgradeable;
+
+    public Laptop(String name, int memory, int storage, int size, boolean isUpgradeable) {
+        super(name, memory, storage);
+        this.size = size;
+        this.isUpgradeable = isUpgradeable;
+    }
+
+    // Getters and Setters
+    public int getSize() {
+        return size;
+    }
+
+    public boolean getIsUpgradeable() {
+        return isUpgradeable;
+    }
+
+    // Override Abstract Methods
+    @Override
+    public void printInfo() {
+        System.out.println("Model: " + getModel());
+        System.out.println("RAM: " + getMemory());
+        System.out.println("Storage " + getStorage());
+        System.out.println("Size " + getSize());
+        System.out.println("Upgradeable: " + getIsUpgradeable());
+    }
+
+    @Override
+    public void upgradeMemory(int newMemory) {
+        if (isUpgradeable) {
+            setMemory(newMemory);
+        } else {
+            System.out.println("Not Upgradeable");
+        }
+    }
+
+    @Override
+    public void upgradeStorage(int newStorage) {
+        if (isUpgradeable) {
+            setStorage(newStorage);
+        } else {
+            System.out.println("Not Upgradeable");
+        }
+    }
 }
