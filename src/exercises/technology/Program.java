2069c2c92235e362e11aa275bce46a49ced3cfa6
Taylor Branson <taylor.a.branson@gmail.com>
2020-08-27 16:41:57 -0500

    implemented Program class

---
+++ src/exercises/technology/Program.java
@@ -0,0 +1,12 @@
+package exercises.technology;
+
+public class Program {
+
+    public static void main(String[] args) {
+        SmartPhone iphoneEight = new SmartPhone("iPhone 8", 4, 64, 4.7, false);
+        iphoneEight.printInfo();
+
+        Laptop asusG14 = new Laptop("Asus Zephyrus G14", 16, 1024, 14, true);
+        asusG14.printInfo();
+    }
+}
