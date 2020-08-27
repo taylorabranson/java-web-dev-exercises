ef66aa7a732df289504fc57a419e6d16d44a2e9a
Taylor Branson <taylor.a.branson@gmail.com>
2020-08-27 16:05:25 -0500

    added Main class

--- src/org/launchcode/java/demos/lsn6inheritance/Main.java
+++ src/org/launchcode/java/demos/lsn6inheritance/Main.java
@@ -1,2 +1,22 @@
-package org.launchcode.java.demos.lsn6inheritance;public class Main {
+package org.launchcode.java.demos.lsn6inheritance;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HouseCat winnifred = new HouseCat("Winnifred", 9);
+        System.out.println(winnifred.purr());
+        System.out.println(winnifred.noise());
+        winnifred.eat();
+        System.out.println(winnifred.isTired());
+        System.out.println(winnifred.isHungry());
+
+        HouseCat leia = new HouseCat("Leia");
+
+        System.out.println(leia.purr());
+        System.out.println(leia.getWeight());
+        leia.sleep();
+        System.out.println(leia.noise());
+
+    }
 }
