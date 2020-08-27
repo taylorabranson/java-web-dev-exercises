5da40a18a0a0c2ad223e1e99cd6d2348254fa8ac
Taylor Branson <taylor.a.branson@gmail.com>
2020-08-27 16:07:10 -0500

    added HouseCatTest, added test for inheritance from super

--- src/org/launchcode/java/demos/lsn6inheritance/test/HouseCatTest.java
+++ src/org/launchcode/java/demos/lsn6inheritance/test/HouseCatTest.java
@@ -1,2 +1,15 @@
-package org.launchcode.java.demos.lsn6inheritance.test;public class HouseCatTest {
+package org.launchcode.java.demos.lsn6inheritance.test;
+
+import org.junit.Test;
+import org.launchcode.java.demos.lsn6inheritance.HouseCat;
+
+import static org.junit.Assert.assertEquals;
+
+public class HouseCatTest {
+
+    @Test
+    public void inheritsSuperInFirstConstructor() {
+        HouseCat keyboardCat = new HouseCat("Keyboard Cat", 7);
+        assertEquals(7,keyboardCat.getWeight(), .001);
+    }
 }
