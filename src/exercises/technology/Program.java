5216920adbdd0b7f5c694fd244aecbd9d015b1c3
Taylor Branson <taylor.a.branson@gmail.com>
2020-08-27 18:40:06 -0500

    commit

--- src/exercises/technology/Program.java
+++ src/exercises/technology/Program.java
@@ -3,7 +3,7 @@ package exercises.technology;
 public class Program {
 
     public static void main(String[] args) {
-        SmartPhone iphoneEight = new SmartPhone("iPhone 8", 4, 64, 4.7, false);
+        SmartPhone iphoneEight = new SmartPhone("iPhone 8", 4, 64, 4.7, false, "4G");
         iphoneEight.printInfo();
 
         Laptop asusG14 = new Laptop("Asus Zephyrus G14", 16, 1024, 14, true);
