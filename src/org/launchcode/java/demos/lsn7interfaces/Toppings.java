2ec15d1cbd4f855e6b3ef11425f6fa7b99628c76
Taylor Branson <taylor.a.branson@gmail.com>
2020-09-12 22:48:04 -0500

    implemented constructor

--- src/org/launchcode/java/demos/lsn7interfaces/Toppings.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Toppings.java
@@ -1,4 +1,10 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
-public class Toppings {
+import java.util.ArrayList;
+
+public class Toppings extends Ingredient{
+
+    public Toppings(String aName, double aCost, ArrayList<String> someAllergens) {
+        super(aName, aCost, someAllergens);
+    }
 }
