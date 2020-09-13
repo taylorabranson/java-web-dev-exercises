6a0a8c953d8be551be29a147f89b0c81ab17883b
Taylor Branson <taylor.a.branson@gmail.com>
2020-09-12 22:41:16 -0500

    commit

--- src/org/launchcode/java/demos/lsn7interfaces/Main.java
+++ src/org/launchcode/java/demos/lsn7interfaces/Main.java
@@ -1,19 +1,30 @@
 package org.launchcode.java.demos.lsn7interfaces;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Main {
     public static void main(String[] args){
         Case menu = new Case();
         ArrayList<Flavor> flavors = menu.getFlavors();
         ArrayList<Cone> cones = menu.getCones();
+        Comparator comparatorFlavor = new FlavorComparator();
+        Comparator comparatorCone = new ConeComparator();
 
-        // TODO: Use a Comparator class to sort the 'flavors' array alphabetically by the 'name'
-        //  field.
+        // Use a Comparator class to sort the 'flavors' array alphabetically by the 'name' field.
+        flavors.sort(comparatorFlavor);
 
-        // TODO: Use a Comparator class to sort the 'cones' array in increasing order by the 'cost'
-        //  field.
+        // Use a Comparator class to sort the 'cones' array in increasing order by the 'cost' field.
+        cones.sort(comparatorCone);
 
-        // TODO: Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+        // Print the 'flavors' and 'cones' lists (in a clear manner) to verify the sorting.
+
+        for (int i = 0; i < flavors.size(); i++) {
+            System.out.println(flavors.get(i).getName());
+        }
+
+        for (int i = 0; i < cones.size(); i++) {
+            System.out.println(cones.get(i).getName() + ": $" + cones.get(i).getCost());
+        }
     }
 }
