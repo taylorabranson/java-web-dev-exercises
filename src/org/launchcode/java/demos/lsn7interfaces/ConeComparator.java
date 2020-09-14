d512e4a824000e40fb520b761a047738928b1905
Taylor Branson <taylor.a.branson@gmail.com>
2020-09-14 18:45:23 -0500

    refactored compare()

--- src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
+++ src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
@@ -5,14 +5,12 @@ import java.util.Comparator;
 public class ConeComparator implements Comparator<Cone> {
     @Override
     public int compare(Cone o1, Cone o2) {
-        int compared = 0;
         if (o1.getCost() < o2.getCost()) {
-            compared = -1;
-        } else if (o1.getCost() == o2.getCost()) {
-            compared = 0;
+            return -1;
         } else if (o1.getCost() > o2.getCost()) {
-            compared = 1;
+            return 1;
+        } else {
+            return 0;
         }
-        return compared;
     }
 }
